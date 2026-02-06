65763ca2bc1aae436446c7950499d941bc860c1d
Taisia Kalinina <taisiak2@illinois.edu>
2026-02-05 21:01:34 -0600

    some more css

--- site/src/components/DonateCard.tsx
+++ site/src/components/DonateCard.tsx
@@ -1,11 +1,14 @@
 import styles from "./DonateCard.module.css";
+import { pressStart } from "@/app/layout";
 
 export default function DonateCard() {
   return (
     <section className={styles.donateSection}>
       <div className={styles.content}>
         <div className={styles.text}>
-          <h2 >QueerCoded_</h2>
+          <h2 className={`${styles.textTitle} ${pressStart.className}`}>
+            QueerCoded_
+          </h2>
         </div>
 
         <a href="/donate" className={styles.donateButton}>
